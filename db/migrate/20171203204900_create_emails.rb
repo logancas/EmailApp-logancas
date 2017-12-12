27f6a94c4c154e2ad46542e7484004a3f3b3ed2d
Logan Castrucci <logancastrucci@gmail.com>
2017-12-12 09:49:48 -0500

    Fixed db issue for heroku

--- db/migrate/20171203204900_create_emails.rb
+++ db/migrate/20171203204900_create_emails.rb
@@ -1,8 +1,8 @@
 class CreateEmails < ActiveRecord::Migration[5.0]
   def change
     create_table :emails do |t|
-      t.references :sender, foreign_key: true
-      t.references :recipient, foreign_key: true
+      t.integer :sender_id, foreign_key: true
+      t.integer :recipient_id, foreign_key: true
       t.string :subject
       t.text :body
       t.string :status
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203205443) do
+ActiveRecord::Schema.define(version: 20171203205055) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer  "email_id"
